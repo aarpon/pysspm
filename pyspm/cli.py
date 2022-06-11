e926daa46a7e4fee9489e1859efa72be57a119b0
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-06-11 22:52:25 +0200

    Initialize Typer and create start script spm.

--- pyspm/cli.py
+++ pyspm/cli.py
@@ -1,5 +1,25 @@
+import sys
+
 import typer
 
+from . import __version__
 
-def main():
-    print(f"spm -- Simple scientific project management tool.")
+# Instantiate Typer
+app = typer.Typer()
+
+
+@app.command()
+def version():
+    """Print version information."""
+    print(f"spm -- Simple scientific project management tool version {__version__}.")
+
+
+@app.command()
+def config():
+    """View and edit configuration options."""
+    print("Allows to visualize and change configuration.")
+
+
+# Entry point for the spm script
+def cli():
+    app()
