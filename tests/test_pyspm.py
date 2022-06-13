7e5ec34c468f474f8fdbcc5b3ea4d88b8101e9ae
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2022-06-13 21:48:16 +0200

    Allow for testing spm cli calls.

--- tests/test_pyspm.py
+++ tests/test_pyspm.py
@@ -1,5 +1,17 @@
+from typer.testing import CliRunner
+
 from pyspm import __version__
+from pyspm.cli import app
+
+# Instantiate a CliRunner object to be able to test the cli app
+runner = CliRunner()
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_app_init():
+    result = runner.invoke(app, ["version"])
+    assert result.exit_code == 0
+    assert result.output == f"{__version__}\n"
